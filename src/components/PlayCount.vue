71eb3e476a8716835b3e7a17849dcba4908c53ca
Naraku777 <442815900@qq.com>
2017-02-25 11:40:00 +0800

    播放量组件

---
+++ src/components/PlayCount.vue
@@ -0,0 +1,42 @@
+<template>
+
+    <span class="play-count" v-show="count">
+        <mu-icon v-if="showIcon" value="hearing"/>
+        {{ count | formatCount }}
+    </span>
+
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+
+            }
+        },
+        props: {
+            count: Number,
+            showIcon: Boolean
+        }
+    }
+
+</script>
+
+<style scoped>
+    .play-count{
+        position: absolute;
+        left: 4px;
+        bottom: 6px;
+        display: flex;
+        align-items: center;
+        font-size: 12px;
+        line-height: 1;
+        color: #fff;
+        text-shadow: 0 0 2px rgba(0, 0, 0, .1)
+    }
+    .play-count .mu-icon{
+        font-size: 12px;
+        margin-right: 4px;
+    }
+</style>
