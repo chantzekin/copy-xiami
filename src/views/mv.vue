91e0f7dced1c4d1327847b0b829aadbc334d32bf
Naraku777 <442815900@qq.com>
2017-02-27 13:58:48 +0800

    mv & radio 待开发

--- src/views/mv.vue
+++ src/views/mv.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="example">{{ msg }}</div>
+    <div class="tip">
+        <mu-icon value="developer_mode"></mu-icon>
+        <span>MV 功能正在开发中...</span>
+    </div>
 </template>
 
 <script>
@@ -10,10 +13,27 @@
             }
         }
     }
+
 </script>
 
-<style>
-    .example {
-        color: red;
+<style lang="scss" scoped>
+    .tip {
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        .mu-icon {
+            font-size: 128px;
+            color: #a8a8a8;
+            margin-bottom: 24px;
+        }
+        span{
+            color: #c8c8c8;
+        }
     }
 </style>
