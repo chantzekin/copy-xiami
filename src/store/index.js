74cc5050b509c885f693c663f4b5ed88df79612f
Naraku777 <442815900@qq.com>
2017-02-28 15:05:28 +0800

    播放器相关状态

--- src/store/index.js
+++ src/store/index.js
@@ -11,20 +11,26 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
     state: {
         audio: {
-            songUrl: 'http://om5.alicdn.com/517/23517/438628/1770182611_2283338_l.mp3?auth_key=3e2868f1122b056b74d569f363cccd5f-1488769200-0-null',
-            coverUrl: 'http://img.xiami.net/images/album/img17/23517/4386281386439629.jpg',
+            id: 0,
             title: '想自由',
             singer: '林宥嘉',
             ablum: '美妙生活',
-            currentLength: 0,
-            songLength: 0,
-            currentFlag: false
+            currentTime: 0,
+            durationTime: 0,
+            bufferedTime: 0,
+            tmpCurrentTime: 0,
+            change: false,
+            songUrl: 'http://om5.alicdn.com/517/23517/438628/1770182611_2283338_l.mp3?auth_key=3e2868f1122b056b74d569f363cccd5f-1488769200-0-null',
+            coverUrl: 'http://img.xiami.net/images/album/img17/23517/4386281386439629.jpg',
         },
         audioLoading: false,
-        isPlay: true,
+        isPlaying: false, // 是否正在播放
         listInfo: {
             songList: [],
             songIndex: 0
+        },
+        player: {
+            isShowPlayerDetail: false
         },
         homeData: {
             banner: HomeData.data.list[0].banner,
@@ -32,10 +38,7 @@ const store = new Vuex.Store({
             collections: HomeData.data.list[2].collections
         },
         collectData: CollectData.data.collects,
-        rankData: RankData.data.common_list,
-        player: {
-            isShowPlayerDetail: false
-        }
+        rankData: RankData.data.common_list
     },
     getters: {
         audio: state => state.audio,
@@ -44,11 +47,33 @@ const store = new Vuex.Store({
         },
         collectData: state => state.collectData,
         rankData: state => state.rankData,
-        player: state => state.player
+        player: state => state.player,
+        isPlaying: state => state.isPlaying,
+        currentTimePercent: state => {
+            return state.audio.currentTime / state.audio.durationTime * 100;
+        },
+        bufferedTimePercent: state => {
+            return state.audio.bufferedTime / state.audio.durationTime * 100;
+        }
     },
     mutations: {
+        play(state) {
+            state.isPlaying = true
+        },
+        pause(state) {
+            state.isPlaying = false
+        },
         togglePlayerDetail: (state, flag) => {
             state.player.isShowPlayerDetail = flag;
+        },
+        updateCurrentTime(state, time) {
+            state.audio.currentTime = time
+        },
+        updateDurationTime(state, time) {
+            state.audio.durationTime = time
+        },
+        updateBufferedTime(state, time) {
+            state.audio.bufferedTime = time
         }
     }
 })
