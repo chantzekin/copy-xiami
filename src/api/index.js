4064294086481cbd9c66178920e7da3ae5864250
Naraku777 <442815900@qq.com>
2017-03-01 10:38:50 +0800

    http / https 问题

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 
 const _baseUrl =
   process.env.NODE_ENV === 'production' ?
-  'http://musicapi.applinzi.com/api/' : 'http://192.168.1.123:8081/api/'
+  '//musicapi.applinzi.com/api/' : '//192.168.1.123:8081/api/'
 
 export default {
   getPlaylist(id) {
